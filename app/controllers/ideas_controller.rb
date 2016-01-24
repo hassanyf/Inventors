7c31340b1584477d560540892879f8f6e7192db5
hassanyf <mrxortax@gmail.com>
2016-01-24 18:58:00 +0500

    Added CRUD ability for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,50 @@
 class IdeasController < ApplicationController
+	before_action :find_idea, only: [:show, :edit, :update, :destroy]
+	
 	def index
+		@ideas = Idea.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@idea = Idea.new
+	end
+
+	def create
+		@idea = Idea.new(idea_params)
+
+		if @idea.save
+			redirect_to @idea
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @idea.update(idea_params)
+			redirect_to @idea
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@idea.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_idea
+		@idea = Idea.find(params[:id])
+	end
+
+	def idea_params
+		params.require(:idea).permit(:title, :link, :description)
 	end
 end
