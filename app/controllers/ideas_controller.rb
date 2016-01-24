4121ce8957ce08025b5815a5b7826b54e2015a68
hassanyf <mrxortax@gmail.com>
2016-01-24 19:21:04 +0500

    Added and set up devise gem

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,6 @@
 class IdeasController < ApplicationController
 	before_action :find_idea, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 	
 	def index
 		@ideas = Idea.all.order("created_at DESC")
@@ -9,11 +10,11 @@ class IdeasController < ApplicationController
 	end
 
 	def new
-		@idea = Idea.new
+		@idea = current_user.ideas.build
 	end
 
 	def create
-		@idea = Idea.new(idea_params)
+		@idea = current_user.ideas.build(idea_params)
 
 		if @idea.save
 			redirect_to @idea
