f8081aaaabdd3441f5ab41300b4a5c161d0a39dc
hassanyf <mrxortax@gmail.com>
2016-02-04 22:35:11 +0500

    Fixed commenting bug with friendly_id and added some styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,12 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	def create
-		@idea = Idea.find(params[:idea_id])
+		@idea = Idea.friendly.find(params[:idea_id])
 		@comment = Comment.create(params[:comment].permit(:content))
 		@comment.user_id = current_user.id
 		@comment.idea_id = @idea.id
 
 		if @comment.save
-			CommentMailer.comment_created(current_user, @idea.user, @comment.content).deliver
 			redirect_to idea_path(@idea)
 		else
 			render 'new'
